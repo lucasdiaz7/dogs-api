6fb106cba1e03da5bbe6928c4d8e3b48e9349cf2
lucasdiaz7 <lucasdiaz687@gmail.com>
2023-02-16 18:48:48 -0300

    x

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const pg= require('pg')
-const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE, PORT, DATABASE_URL } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE, PORT } = process.env;
 
 
 const sequelize = new Sequelize(
