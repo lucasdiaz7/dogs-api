e5b327bd94a7b44b30931a4f485fcf1486e182e0
lucasdiaz7 <lucasdiaz687@gmail.com>
2023-02-16 18:37:07 -0300

    cambiando cosas del deploy

--- src/db.js
+++ src/db.js
@@ -2,22 +2,23 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE, DATABASE_URL } = process.env;
+const pg= require('pg')
+const { DB_USER, DB_PASSWORD, DB_HOST, DATABASE, PORT, DATABASE_URL } = process.env;
 
 
 const sequelize = new Sequelize(
-  // postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DATABASE},
-  process.env.DATABASE_URL,
+    `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${PORT}/${DATABASE}`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
     timestamps: false,
-    dialectOptions: {
-      ssl: {
-        required: true,
-        rejectUnauthorized: false,
-      },
-    },
+    dialectModule: pg,
+    // dialectOptions: {
+    //   ssl: {
+    //     required: true,
+    //     rejectUnauthorized: false,
+    //   },
+    // },
   }
 );
 const basename = path.basename(__filename);
